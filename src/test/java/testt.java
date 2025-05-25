bd0d847efebe0f011ee0d63bac59f88d88f59341
Kloping <3474006766@qq.com>
2025-05-25 21:51:16 +0800

    feat(conf): 添加开放的 GID 配置并更新相关功能- 在 conf.txt 中添加 open.gid 配置项，指定开放的 GID 列表 - 修改 testt.java，增加 CountDownLatch 相关代码，用于异步处理 - 新增 run.sh脚本，用于编译和运行项目

--- src/test/java/testt.java
+++ src/test/java/testt.java
@@ -1,12 +1,18 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author github kloping
  * @date 2025/4/24-14:06
  */
 public class testt {
     public static void main(String[] args) {
-        System.out.println((int) "①".charAt(0));
-        System.out.println((int) "②".charAt(0));
-        System.out.println((int) "③".charAt(0));
-        System.out.println(String.valueOf((char) ((char) 9311 + 1)));
+        CountDownLatch cdl = new CountDownLatch(1);
+        try {
+            boolean k = cdl.await(1, TimeUnit.SECONDS);
+            System.out.println(k);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

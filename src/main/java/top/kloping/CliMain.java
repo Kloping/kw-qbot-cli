2d077785125b180af956b26e97cb30cffe2719c2
Kloping <3474006766@qq.com>
2025-08-05 12:14:05 +0800

    refactor: 调整应用等待时间
    - 将 APPLICATION.setWaitTime 方法的参数从 20000 修改为 45000
    - 此修改增加了应用的等待时间，可能为了适应更复杂的初始化过程或网络环境

--- src/main/java/top/kloping/CliMain.java
+++ src/main/java/top/kloping/CliMain.java
@@ -87,7 +87,7 @@ public class CliMain implements ListenerHost, Runner {
             APPLICATION = new StarterObjectApplication(CliMain.class);
             APPLICATION.setAccessTypes(MessageEvent.class, Long.class);
             APPLICATION.setMainKey(Long.class);
-            APPLICATION.setWaitTime(20000);
+            APPLICATION.setWaitTime(45000);
             APPLICATION.setAllAfter(INSTANCE);
             APPLICATION.addConfFile("./conf/conf.txt");
             APPLICATION.logger = LoggerImpl.INSTANCE;
